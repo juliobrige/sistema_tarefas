db25453bcaf58723d7b6fdc0f94b9f0b4266961c
juliobrige <juliobenjamimbernado36@gmail.com>
2025-06-04 08:07:28 -0300

    model

--- tarefas/admin.py
+++ tarefas/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Tarefa, Categoria
+
+admin.site.register(Tarefa)
+admin.site.register(Categoria)
 
 # Register your models here.
--- tarefas/models.py
+++ tarefas/models.py
@@ -1,3 +1,27 @@
 from django.db import models
 
+class Categoria(models.Model):
+    nome = models.CharField(max_length=50)
+    cor = models.CharField(max_length=20, default='gray')
+
+    def __str__(self):
+        return self.nome
+
+PRIORIDADES = [
+    ('alta', '🔥 Alta'),
+    ('media', '🟡 Média'),
+    ('baixa', '🟢 Baixa'),
+]
+
+class Tarefa(models.Model):
+    descricao = models.CharField(max_length=200)
+    concluida = models.BooleanField(default=False)
+    agendada_para = models.DateTimeField(null=True, blank=True)
+    prioridade = models.CharField(max_length=10, choices=PRIORIDADES, default='media')
+    categoria = models.ForeignKey(Categoria, on_delete=models.SET_NULL, null=True, blank=True)
+    mudancas_data = models.IntegerField(default=0)
+
+    def __str__(self):
+        return self.descricao
+
 # Create your models here.
